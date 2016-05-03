2066c67b7e4cea389a52eee1800fef5341583973
beatrizacbs <beatrizacbs@gmail.com>
2016-05-03 20:46:20 -0300

    Melhoramento de endereço e criação do controlador cliente

---
+++ src/LF/Cliente/ControladorCilente.java
@@ -0,0 +1,13 @@
+package LF.Cliente;
+
+public class ControladorCilente {
+	
+	private RepositorioCliente reposditorioCliente;
+	
+	public boolean atualizarEndereco(String logradouro, String bairro, String cep, String numero, String cidade, String estado, String complemento)
+	{
+		
+		return true;
+	}
+
+}
